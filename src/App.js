86ca31a4d9780ade8ca4fcab8e17bca1fbda9f22
Roman <rozario91208@gmail.com>
2019-07-14 14:40:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+function App() { 
+  return ( 
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
