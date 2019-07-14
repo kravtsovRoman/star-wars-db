2ac86c980a94a05c51199c80d62ad73000bdd807
Roman <rozario91208@gmail.com>
2019-07-14 15:26:16 +0300

    fetch query and clear project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const getResource = async (url) => {
+    const res = await fetch(url);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    if (!res.ok) {
+        throw new Error(`Could nor fetch ${url}, received ${res.status}`)
+    }
+    const body = await res.json();
+    return body;
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+getResource('https://swapi.co/api/people/999s/')
+    .then((body) => {
+        console.log(body);
+    })
+    .catch((err) => {
+        console.log('Could not fetch', err)
+    })
