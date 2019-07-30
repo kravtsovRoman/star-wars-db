7b18d53edf2f41987b9ee88ff1333a7abf3223ca
Roman <rozario91208@gmail.com>
2019-07-30 23:06:30 +0300

    Fix

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -18,7 +18,6 @@ export default class RandomPlanet extends Component {
   componentDidMount() {
     this.updatePlanet();
     this.interval = setInterval(this.updatePlanet, 10000);
-
   }
 
   componentWillUnmount() {
