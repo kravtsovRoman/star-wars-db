ccc348cfe06cc53f5cfd82815d18cb34902dbaa0
Roman <rozario91208@gmail.com>
2019-08-04 19:32:07 +0300

    output another items

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,32 +1,31 @@
 import React, { Component } from 'react';
 
 import './item-list.css';
-import SwapiService from "../../services/swapi-service";
 import Spinner from "../spinner/spinner";
 
 export default class ItemList extends Component {
 
-  swapiService = new SwapiService();
-
   state = {
-    peopleList: null
+    itemList: null
   };
 
   componentDidMount() {
-    this.swapiService.getAllPeople()
-      .then((peopleList) => {
+
+    const { getData } = this.props;
+    getData()
+      .then((itemList) => {
         this.setState({
-          peopleList
+          itemList
         });
       });
   }
 
   renderItems(arr) {
-    return arr.map(({id, name}) => {
+    return arr.map(({ id, name }) => {
       return (
         <li className="list-group-item"
-            key={id}
-            onClick={() => this.props.onItemSelected(id)}>
+          key={id}
+          onClick={() => this.props.onItemSelected(id)}>
           {name}
         </li>
       );
@@ -35,13 +34,13 @@ export default class ItemList extends Component {
 
   render() {
 
-    const { peopleList } = this.state;
+    const { itemList } = this.state;
 
-    if (!peopleList) {
+    if (!itemList) {
       return <Spinner />;
     }
 
-    const items = this.renderItems(peopleList);
+    const items = this.renderItems(itemList);
 
     return (
       <ul className="item-list list-group">
