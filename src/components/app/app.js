8054ea54e6a2f5cfc00aaff144a69e1fe04e7228
Roman <rozario91208@gmail.com>
2019-08-27 21:58:39 +0300

    withRouter again

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -42,7 +42,7 @@ export default class App extends Component {
               <RandomPlanet />
 
               <Route path="/" render={() => <h2>Welcome to StarDB</h2>} exact />
-              <Route path="/people" component={PeoplePage} />
+              <Route path="/people/:id?" component={PeoplePage} />
               <Route path="/planets" component={PlanetsPage} />
               <Route path="/starships" component={StarshipsPage} exact />
               <Route path="/starships/:id"
